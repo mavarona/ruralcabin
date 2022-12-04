28b291c9b1310663582ee295e555ed02cb9501ef
mavl <mvaronamac@gmail.com>
2022-12-04 21:52:19 +0100

    Add icon phone

--- components/booking/bookingFilter.js
+++ components/booking/bookingFilter.js
@@ -9,7 +9,11 @@ export default function BookingFilter({cabins, styles, message, locale, onChange
             <section className={styles.filterContainer}>
                 <Title style={styles.title} 
                        text={message.booking} />
-                <div className={styles.titleProvisional}>Si quiere agilizar su reserva llamar al <div className={styles.titleProvisionalLink} onClick={callPhone}>620419157</div></div>
+                <div className={styles.titleProvisional}>Si quiere agilizar su reserva</div>
+                <div className={styles.titleProvisional} onClick={callPhone}>
+                    <i className="fas fa-phone-volume fa-2x"></i>
+                    <span className={styles.titleProvisionalLink}>620419157</span>
+                </div>
             </section>
         </>
     )
