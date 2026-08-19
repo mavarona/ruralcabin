f985e8f393e394380e2de6e17d6212b32c220299
Miguel Ángel <mvaronamac@gmail.com>
2026-08-20 00:59:37 +0200

    Fix JSON stringify method for calendar data

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -5,7 +5,7 @@ export async function allCalendar() {
     try {
         dbConnect()
         const calendar = await Calendar.find({})
-        return JSON.parse(stringify(calendar))
+        return JSON.parse(JSON.stringify(calendar))
     } catch (error) {
         return null
     }
