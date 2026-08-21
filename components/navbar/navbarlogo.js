d25ca7a374bbbef383f0203d527014048ee31740
Miguel Ángel <mvaronamac@gmail.com>
2026-08-21 23:54:04 +0200

    Update NavbarLogo to use img instead of Image
    Replaced Image component with standard img tag in NavbarLogo.

--- components/navbar/navbarlogo.js
+++ components/navbar/navbarlogo.js
@@ -1,17 +1,12 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
-export default function NavbarLogo({styles, path, image, altText}) {
-    
-    return (
-        <Link href={path}>
-            <a className={styles.navlogoMain}>
-                <Image src={image}
-                    width={200}
-                    height={50}
-                    alt={altText} />
-            </a>
-        </Link>
-    )
-                      
+export default function NavbarLogo({ styles, path, image, altText }) {
+
+  return (
+    <Link href={path}>
+      <a className={styles.navlogoMain}>
+        <img src={image} width="200" height="50" alt={altText} />
+      </a>
+    </Link>
+  )
 }
